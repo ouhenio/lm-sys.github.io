46de3246ce97b3d81fbab345ae5c4fd79724649c
Ying Sheng <sqy1415@gmail.com>
2023-11-15 10:10:24 -0800

    remove mathjax

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="stylesheet" href="/fonts/load.css" />
-        <script src="/mathjax.js" async></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.0/es5/tex-mml-chtml.min.js" async></script>
       </Head>
       <body>
         <Main />
